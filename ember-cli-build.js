85147dabb64e8ac2d2d8dcb4503c65f1b678c64a
Joseph Daniels <joedaniels29@me.com>
2016-09-06 10:34:41 -0400

    add Diff match patch

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -13,6 +13,9 @@ module.exports = function(defaults) {
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
-
+  app.import({
+      development: "bower_components/google-diff-match-patch/diff_match_patch_uncompressed.js",
+      production: "bower_components/google-diff-match-patch/diff_match_patch.js"
+  });
   return app.toTree();
 };
